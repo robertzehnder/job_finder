764c9a276f2deb73b60ca3131b9686a2d1fe9684
robertzehnder <robzcodes@gmail.com>
2018-11-07 22:29:48 -0500

    reaches polls index successfully

--- job_finder/job_finder/urls.py
+++ job_finder/job_finder/urls.py
@@ -14,13 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
-
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
